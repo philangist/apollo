86dfdf0de9fbf1d33537169f6536d04290de2b1a
Phil Opaola <phillipsopaolacoding@gmail.com>
2018-06-02 06:14:40 -0400

    add PollTransactions to Batch and the notion of a Mixer

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 var pool *JobcoinWallet
@@ -32,7 +33,6 @@ func (j *JobcoinWallet) Send(recipient Address, amount int) error {
 	}
 
 	fmt.Printf("Sending amount '%d' to recipient '%s'\n", amount, recipient)
-
 	return nil
 }
 
@@ -61,7 +61,50 @@ func (b *Batch) Execute () (err error) {
 	return err
 }
 
-func main (){}
+func (b *Batch) PollTransactions() chan bool {
+	ready := make(chan bool, 1)
+
+	go func(){
+		time.Sleep(1 * time.Second)
+		fmt.Printf("PollTransactions() finished for batch %v\n", b)
+		ready <- true
+	}()
+	fmt.Println("PollTransactions() early exit")
+	return ready
+}
+
+type Mixer struct {
+	Batches []*Batch
+}
+
+func (p *Mixer) Run() (err error){
+	for _, b := range p.Batches {
+		// go func(b *Batch){
+		ready := b.PollTransactions()
+		fmt.Printf("Blocked on batch %v's ready channel\n", b)
+		<- ready
+		fmt.Printf("Unblocked on batch %v's ready channel\n", b)
+		b.Execute()
+		// }(b)
+	}
+	return err
+}
+
+func main (){
+	batch := &Batch{
+		10,
+		2,
+		[]Address{
+			Address("Address-1"),
+		},
+		[]Address{
+			Address("Address-1"), Address("Address-2"), 
+		},
+	}
+	batches := []*Batch{batch}
+	mixer := &Mixer{batches}
+	mixer.Run()
+}
 
 func init(){
 	pool = NewJobcoinWallet("Pool")

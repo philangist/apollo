859a3c0f6507ecabc129624a9a9a6b51a06011ae
Phil Opaola <phillipsopaolacoding@gmail.com>
2018-06-02 04:34:28 -0400

    start defining core types & interfaces

--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
+import "net/http"
+
+type Address string
+
+type Wallet interface {
+	Address() *Address
+	Send(recipient *Address, amount int) error
+}
+
+
+type JobcoinWallet struct {
+	client *http.Client
+	address Address
+}
+
+func NewJobcoinWallet(address string) *JobcoinWallet {
+	return &JobcoinWallet{
+		client: &http.Client{},
+		address: Address(address),
+	}
+}
+
+func (j *JobcoinWallet) Send(recipient Address, amount int) error {
+	// enforce amount is gte zero
+	return nil
+}
+
+func (j *JobcoinWallet) Address() Address {
+	return j.address
+}
+
+type Batch struct {
+	Amount int
+	Fee int
+	Sources []Address
+	Recipients []Address
+}
+
+func (b *Batch) Execute () {}
+
 func main (){
 
 }
--- main_test.go
+++ main_test.go
@@ -5,9 +5,26 @@ import (
 	"testing"
 )
 
-func TestPlaceholder(t *testing.T){
-	fmt.Println("Running TestPlaceholder...")
-	if 1 != 1 {
-		t.Error("1 != 1")
+func TestCreateWallet(t *testing.T){
+	fmt.Println("Running TestCreateWallet...")
+
+	j := NewJobcoinWallet("Alice")
+	expected := Address("Alice")
+
+	if j.Address() != expected {
+		t.Errorf("Jobcoin wallet was not created with expected address 'Alice'. Received '%s' instead", j.Address())
+	}
+}
+
+func TestWalletSend(t *testing.T){
+	fmt.Println("Running TestWalletSend...")
+
+	j := NewJobcoinWallet("Alice")
+	b := Address("Bob")
+
+	err := j.Send(b, 100)
+
+	if err != nil {
+		t.Errorf("Jobcoin wallet did not successfully send amount '100' to recipient 'Bob'. Saw error '%s' instead", err)
 	}
 }
